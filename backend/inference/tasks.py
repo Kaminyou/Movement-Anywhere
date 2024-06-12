1c3ecbb2c4443fa513739cb543b3b4b29a7e842f
Kaminyou <ikaminyou@gmail.com>
2024-06-13 03:09:47 +0800

    update main task celery setting

--- backend/inference/tasks.py
+++ backend/inference/tasks.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from redis import Redis
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -10,8 +11,9 @@ from models import RequestModel
 from .gait import inference_gait
 
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
-CELERY_BACKEND_URL = os.environ.get("CELERY_BACKEND_URL", "redis://redis:6379")
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
+CELERY_BACKEND_URL = os.environ.get('CELERY_BACKEND_URL')
+TASK_SYNC_URL = os.environ.get('TASK_SYNC_URL')
 
 app = Celery(
     'tasks',
@@ -22,12 +24,25 @@ app = Celery(
 )
 
 
-@app.task(bind=True, name='inference_gait_task', default_retry_delay=60)
+@app.task(
+    bind=True,
+    name='inference_gait_task',
+    queue='inference_gait_task_queue',
+    default_retry_delay=60,
+)
 def inference_gait_task(self, submitUUID: str):
     print(submitUUID)
     """
     The gait inference process
     """
+
+    redis = Redis.from_url(TASK_SYNC_URL)
+    key = f'entry_task_{submitUUID}'
+    if redis.exists(key):
+        print(f'Skip this task since {key} exists')
+        return True
+    redis.set(key, 1)
+
     engine = create_engine(
         os.getenv(
             'SQLALCHEMY_DATABASE_URI',
